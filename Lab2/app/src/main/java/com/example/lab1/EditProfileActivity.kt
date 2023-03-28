9ed40eaf730071bf757fa85ed2246c4b412ee46b
gio4141 <100480700+gio4141@users.noreply.github.com>
2023-03-28 23:18:57 +0200

    add element in res folder

--- Lab2/app/src/main/java/com/example/lab1/EditProfileActivity.kt
+++ Lab2/app/src/main/java/com/example/lab1/EditProfileActivity.kt
@@ -1,11 +1,35 @@
 package com.example.lab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_edit_profile)
+
+        val fullName:TextView = findViewById(R.id.full_name_user_profile)
+
+        fullName.visibility = View.GONE
+
+        val arrowBack: ImageView = findViewById(R.id.arrow_back_user_profile)
+
+        arrowBack.visibility = View.VISIBLE
+
+        arrowBack.setOnClickListener {
+            //load second activity
+            val intent = Intent(this, ShowProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
+
 }
--- Lab2/app/src/main/java/com/example/lab1/ShowProfileActivity.kt
+++ Lab2/app/src/main/java/com/example/lab1/ShowProfileActivity.kt
@@ -3,7 +3,7 @@ package com.example.lab1
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
+import android.view.Window
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,37 +11,38 @@ import androidx.appcompat.app.AppCompatActivity
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); //delete default app title
 
         // Load the appropriate layout file based on the device orientation
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
             setContentView(R.layout.activity_main)
-        } else {
+        else
             setContentView(R.layout.activity_main)
-        }
 
-        val editIcon: ImageView = findViewById(R.id.edit_icon)
+
+
+        val editIcon: ImageView = findViewById(R.id.edit_icon_user_profile)
         editIcon.setOnClickListener {
             //load second activity
-            goToAnActivity(it)
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
         }
+
+
         
     }
 
-    fun goToAnActivity(view: View?) {
-        val intent = Intent(this, EditProfileActivity::class.java)
-        startActivity(intent)
-    }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
 
         // Reload the appropriate layout file when the device orientation changes
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
             setContentView(R.layout.activity_main)
-        } else {
+        else
             setContentView(R.layout.activity_main)
-        }
+
     }
 
 
